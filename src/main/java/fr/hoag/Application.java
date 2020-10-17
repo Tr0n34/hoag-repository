ee9b74adec8c2056947ccc6ab6af75d02ab71355
nletteron <nicolas.letteron.fr@gmail.com>
2020-10-17 11:49:02 +0200

    Initial commit

---
+++ src/main/java/fr/hoag/Application.java
@@ -0,0 +1,7 @@
+package fr.hoag;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+}
---
+++ src/main/java/fr/hoag/controllers/ConfigurationController.java
@@ -0,0 +1,4 @@
+package fr.hoag.controllers;
+
+public class ConfigurationController {
+}
